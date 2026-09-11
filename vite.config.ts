6d2b1622503461b8c98fb2610abb24c7709acc48
technical@cses.carleton.ca <technical@cses.carleton.ca>
2026-09-11 16:28:26 -0400

    Allow egov.mycses.ca host in both server and preview Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['egov.mycses.ca'],
+  },
   preview: {
-    allowedHosts: ['egov.mycses.ca']
+    allowedHosts: ['egov.mycses.ca'],
   }
 })
